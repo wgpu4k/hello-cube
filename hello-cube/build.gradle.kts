f6a53b42b85c4ff656efed2946dfa86d6b486a43
ygdrasil-io <alexandre.mommers@gmail.com>
2024-09-13 18:58:39 +0200

    Add iOS support and update Gradle wrapper version
    Implemented iOS application entry point with a main function and AppDelegate class. Adjusted iOS simulator tasks in the Gradle configuration. Reverted the Gradle wrapper version to 8.9 for compatibility.

--- hello-cube/build.gradle.kts
+++ hello-cube/build.gradle.kts
@@ -2,146 +2,149 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
-	alias(libs.plugins.kotlinMultiplatform)
-	//id("org.jetbrains.gradle.apple.applePlugin") version "222.4595-0.23.2"
+    alias(libs.plugins.kotlinMultiplatform)
+    //id("org.jetbrains.gradle.apple.applePlugin") version "222.4595-0.23.2"
 }
 
 java {
-	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(22))
-	}
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(22))
+    }
 }
 
 kotlin {
-	js {
-		binaries.executable()
-		browser()
-	}
+    js {
+        binaries.executable()
+        browser()
+    }
 
-	jvm {
-		// On to make "JavaExec" work, else we got SourceSet with name 'main' not found, see https://youtrack.jetbrains.com/issue/KT-42683
-		withJava()
-	}
+    jvm {
+        // On to make "JavaExec" work, else we got SourceSet with name 'main' not found, see https://youtrack.jetbrains.com/issue/KT-42683
+        withJava()
+    }
 
-	@OptIn(ExperimentalWasmDsl::class)
-	wasmJs {
-		binaries.executable()
-		browser()
-	}
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        binaries.executable()
+        browser()
+    }
 
-	listOf(
-		iosX64(),
-		iosArm64(),
-		iosSimulatorArm64()
-	).forEach {
-		it.binaries {
-			framework {
-				baseName = "shared"
-			}
-			executable {
-				entryPoint = "main"
-			}
-		}
-	}
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries {
+            framework {
+                baseName = "shared"
+            }
+            executable {
+                entryPoint = "main"
+            }
+        }
+    }
 
-	sourceSets {
+    sourceSets {
 
-		val commonMain by getting {
-			dependencies {
-				implementation(projects.shared)
-			}
-		}
+        val commonMain by getting {
+            dependencies {
+                implementation(projects.shared)
+            }
+        }
 
-	}
+    }
 
-	@OptIn(ExperimentalKotlinGradlePluginApi::class)
-	compilerOptions {
-		allWarningsAsErrors = true
-	}
+    @OptIn(ExperimentalKotlinGradlePluginApi::class)
+    compilerOptions {
+        allWarningsAsErrors = true
+    }
 }
 
 enum class Os {
-	Linux,
-	Windows,
-	MacOs
+    Linux,
+    Windows,
+    MacOs
 }
 
 enum class Architecture {
-	X86_64,
-	AARCH64
+    X86_64,
+    AARCH64
 }
 
 object Platform {
-	val os: Os
-		get() = System.getProperty("os.name").let { name ->
-			when {
-				arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } -> Os.Linux
-				arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) } -> Os.MacOs
-				arrayOf("Windows").any { name.startsWith(it) } -> Os.Windows
-				else -> error("Unrecognized or unsupported operating system.")
-			}
-		}
+    val os: Os
+        get() = System.getProperty("os.name").let { name ->
+            when {
+                arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } -> Os.Linux
+                arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) } -> Os.MacOs
+                arrayOf("Windows").any { name.startsWith(it) } -> Os.Windows
+                else -> error("Unrecognized or unsupported operating system.")
+            }
+        }
 
-	val architecture: Architecture
-		get() = System.getProperty("os.arch").let { architecture ->
-			when (architecture) {
-				"aarch64" -> Architecture.AARCH64
-				"x86_64", "amd64" -> Architecture.X86_64
-				else -> error("Unrecognized or unsupported architecture $architecture.")
-			}
-		}
+    val architecture: Architecture
+        get() = System.getProperty("os.arch").let { architecture ->
+            when (architecture) {
+                "aarch64" -> Architecture.AARCH64
+                "x86_64", "amd64" -> Architecture.X86_64
+                else -> error("Unrecognized or unsupported architecture $architecture.")
+            }
+        }
 
 }
 
 tasks.register<JavaExec>("runApp") {
-	mainClass = "MainKt"
-	if (Platform.os == Os.MacOs) {
-		jvmArgs(
-			"-XstartOnFirstThread",
-			"--add-opens=java.base/java.lang=ALL-UNNAMED",
-			"--enable-native-access=ALL-UNNAMED"
-		)
-	} else {
-		jvmArgs(
-			"--add-opens=java.base/java.lang=ALL-UNNAMED",
-			"--enable-native-access=ALL-UNNAMED"
-		)
-	}
-	classpath = sourceSets["main"].runtimeClasspath
+    mainClass = "MainKt"
+    if (Platform.os == Os.MacOs) {
+        jvmArgs(
+            "-XstartOnFirstThread",
+            "--add-opens=java.base/java.lang=ALL-UNNAMED",
+            "--enable-native-access=ALL-UNNAMED"
+        )
+    } else {
+        jvmArgs(
+            "--add-opens=java.base/java.lang=ALL-UNNAMED",
+            "--enable-native-access=ALL-UNNAMED"
+        )
+    }
+    classpath = sourceSets["main"].runtimeClasspath
 }
 
 java {
-	toolchain {
-		languageVersion.set(JavaLanguageVersion.of(22))
-	}
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(22))
+    }
 }
 
 
 if (Platform.os == Os.MacOs) {
-	project.tasks.register<Exec>("runIosSim") {
-		val device = "iPhone 15"
-		workingDir = project.buildDir
-		val linkExecutableTaskName = when (Platform.architecture) {
-			Architecture.X86_64 -> "linkReleaseExecutableIosX64"
-			Architecture.AARCH64 -> "linkReleaseExecutableIosSimulatorArm64"
-		}
-		val binTask = project.tasks.named(linkExecutableTaskName)
-		dependsOn(binTask)
-		commandLine = listOf(
-			"xcrun",
-			"simctl",
-			"spawn",
-			"--standalone",
-			device
-		)
+    project.tasks.register<Exec>("runIosSim") {
+        val device = "iPhone 15"
+        workingDir = project.buildDir
+        val linkExecutableTaskName = when (Platform.architecture) {
+            Architecture.X86_64 -> "linkReleaseExecutableIosX64"
+            Architecture.AARCH64 -> "linkReleaseExecutableIosSimulatorArm64"
+        }
+        val binTask = project.tasks.named(linkExecutableTaskName)
+        dependsOn(binTask)
+        commandLine = listOf(
+            "xcrun",
+            "simctl",
+            "spawn",
+            "--standalone",
+            device
+        )
 
-		println("file ${binTask.get().outputs.files.single()}")
-		argumentProviders.add {
-			val out = fileTree(binTask.get().outputs.files.files.single()) { include("*.kexe") }
-			println("out $out")
-			listOf(out.single { it.name.endsWith(".kexe") }.absolutePath)
-		}
-	}
+        println("file ${binTask.get().outputs.files.single()}")
+        argumentProviders.add {
+            binTask.get().outputs.files.files.single()
+                .listFiles()?.firstOrNull { it.extension == "kexe" }
+                ?.absolutePath
+                ?.let { listOf(it) }
+                ?.also { println("out $it") }
+                ?: error("No kexe found")
+        }
+    }
 }
 /*
 apple {
--- hello-cube/src/iosMain/kotlin/main.kt
+++ hello-cube/src/iosMain/kotlin/main.kt
@@ -1,4 +1,59 @@
+@file:OptIn(ExperimentalForeignApi::class, BetaInteropApi::class, ExperimentalObjCName::class)
 
-fun main() {
-    println("its alive !")
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.autoreleasepool
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.toCStringArray
+import platform.Foundation.NSStringFromClass
+import platform.UIKit.UIApplication
+import platform.UIKit.UIApplicationDelegateProtocol
+import platform.UIKit.UIApplicationDelegateProtocolMeta
+import platform.UIKit.UIApplicationMain
+import platform.UIKit.UIResponder
+import platform.UIKit.UIResponderMeta
+import platform.UIKit.UIScreen
+import platform.UIKit.UIViewController
+import platform.UIKit.UIWindow
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
+import kotlin.experimental.ExperimentalObjCName
+
+fun main(args: Array<String>) {
+    println("Hello cube")
+    memScoped {
+        val argc = args.size + 1
+        val argv = (arrayOf("konan") + args).toCStringArray(memScope)
+        autoreleasepool {
+            println("Hello cube ${args.joinToString()}}")
+            UIApplicationMain(argc, argv, null, NSStringFromClass(AppDelegate))
+        }
+    }
+}
+
+class AppDelegate : UIResponder, UIApplicationDelegateProtocol {
+    companion object : UIResponderMeta(), UIApplicationDelegateProtocolMeta {}
+
+    private var _window: UIWindow? = null
+
+    @OverrideInit
+    constructor() : super()
+
+    override fun application(
+        application: UIApplication,
+        didFinishLaunchingWithOptions: Map<Any?, *>?
+    ): Boolean {
+        println("Hello cube")
+        val window = UIWindow(frame = UIScreen.mainScreen.bounds)
+        setWindow(window)
+
+
+        return true
+    }
+
+    override fun window() = _window
+
+    override fun setWindow(window: UIWindow?) {
+        _window = window
+    }
 }
