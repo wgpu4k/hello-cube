4ebe8e12f1aa978964a1869fbbf2c2bf10b72c5c
ygdrasil-io <alexandre.mommers@gmail.com>
2024-09-18 12:44:37 +0200

    Add activity-compose dependency via libs.versions.toml
    Replaced hardcoded activity-compose dependency with a reference from libs.versions.toml. This change improves maintainability by centralizing version management.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -17,9 +17,7 @@ kotlin {
     sourceSets {
         androidMain.dependencies {
             api(projects.shared)
-            implementation("androidx.core:core-ktx:1.7.0")
-            implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-            implementation("androidx.activity:activity-compose:1.3.1")
+            implementation(libs.activity.compose)
         }
     }
 }
