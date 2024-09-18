dc9a4098503a08cf5bf919eabe4fa3152d04c0d5
ygdrasil-io <alexandre.mommers@gmail.com>
2024-09-18 12:33:05 +0200

    Refactor Kotlin Multiplatform build configuration
    Updated JVM target to JVM 22 and improved native target detection. Removed commented code and redundant task registration for iOS simulator.

--- hello-cube/build.gradle.kts
+++ hello-cube/build.gradle.kts
@@ -1,10 +1,9 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.apple.XCFramework
-import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    //id("org.jetbrains.gradle.apple.applePlugin") version "222.4595-0.23.2"
 }
 
 java {
@@ -22,9 +21,13 @@ kotlin {
     jvm {
         // On to make "JavaExec" work, else we got SourceSet with name 'main' not found, see https://youtrack.jetbrains.com/issue/KT-42683
         withJava()
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_22
+        }
     }
 
-    @OptIn(ExperimentalWasmDsl::class)
+    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
     wasmJs {
         binaries.executable()
         browser()
@@ -46,15 +49,13 @@ kotlin {
         }
     }
 
-    val hostOs = System.getProperty("os.name")
-    val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val nativeTarget = when {
-        hostOs == "Mac OS X" && isArm64 -> macosArm64("osx")
-        hostOs == "Mac OS X" && !isArm64 -> macosX64("osx")
+    when(Platform.os) {
+         Os.MacOs -> when(Platform.architecture) {
+             Architecture.X86_64 -> macosArm64("osx")
+             Architecture.AARCH64 -> macosX64("osx")
+         }
         else -> null // Not supported
-    }
-
-    if (nativeTarget != null) {
+    }?.let { nativeTarget ->
         with(nativeTarget) {
 
             binaries {
@@ -64,7 +65,6 @@ kotlin {
             }
         }
     }
-
 
     sourceSets {
 
@@ -137,44 +137,3 @@ java {
         languageVersion.set(JavaLanguageVersion.of(22))
     }
 }
-
-
-if (Platform.os == Os.MacOs) {
-    project.tasks.register<Exec>("runIosSim") {
-        val device = "iPhone 15"
-        workingDir = project.buildDir
-        val linkExecutableTaskName = when (Platform.architecture) {
-            Architecture.X86_64 -> "linkReleaseExecutableIosX64"
-            Architecture.AARCH64 -> "linkReleaseExecutableIosSimulatorArm64"
-        }
-        val binTask = project.tasks.named(linkExecutableTaskName)
-        dependsOn(binTask)
-        commandLine = listOf(
-            "xcrun",
-            "simctl",
-            "spawn",
-            "--standalone",
-            device
-        )
-
-        println("file ${binTask.get().outputs.files.single()}")
-        argumentProviders.add {
-            binTask.get().outputs.files.files.single()
-                .listFiles()?.firstOrNull { it.extension == "kexe" }
-                ?.absolutePath
-                ?.let { listOf(it) }
-                ?.also { println("out $it") }
-                ?: error("No kexe found")
-        }
-    }
-}
-/*
-apple {
-	iosApp {
-		productName = "HelloCube"
-		sceneDelegateClass = "SceneDelegate"
-		dependencies {
-			implementation(project(":"))
-		}
-	}
-}*/
